ca16565206d7889074b4a85452f3e0f96d442f83
ArtemiyYarovenko <tekkenik@mail.ru>
2020-12-06 22:32:07 +0700

    Добавление меню 3

--- app/src/main/java/a/event_handling/ButtonActivity.java
+++ app/src/main/java/a/event_handling/ButtonActivity.java
@@ -1,4 +1,14 @@
 package a.event_handling;
 
-class ButtonActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button);
+    }
 }
