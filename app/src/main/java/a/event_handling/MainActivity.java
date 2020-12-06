4b9632ffb43a1b3abf50f7f4950eab3d8cc14076
ArtemiyYarovenko <tekkenik@mail.ru>
2020-12-06 22:09:39 +0700

    Жесты

--- app/src/main/java/a/event_handling/MainActivity.java
+++ app/src/main/java/a/event_handling/MainActivity.java
@@ -2,14 +2,25 @@ package a.event_handling;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.GestureDetectorCompat;
 
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
-public class MainActivity extends AppCompatActivity {
+
+
+public class MainActivity extends AppCompatActivity
+        implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener
+{
+
+    private TextView gestureText;
+    private GestureDetectorCompat gDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +29,10 @@ public class MainActivity extends AppCompatActivity {
 
         ConstraintLayout myLayout = findViewById(R.id.activity_main);
         Button button = findViewById(R.id.button);
+        gestureText = findViewById(R.id.textView);
+        this.gDetector = new GestureDetectorCompat(this,this);
+        gDetector.setOnDoubleTapListener(this);
+
 
         myLayout.setOnTouchListener(new ConstraintLayout.OnTouchListener() {
             public boolean onTouch(View v, MotionEvent m) {
@@ -84,4 +99,66 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        gestureText.setText ("onDown");
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        gestureText.setText("onFling");
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        gestureText.setText("onLongPress");
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                            float distanceX, float distanceY) {
+        gestureText.setText("onScroll");
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        gestureText.setText("onShowPress");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        gestureText.setText("onSingleTapUp");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        gestureText.setText("onDoubleTap");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        gestureText.setText("onDoubleTapEvent");
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        gestureText.setText("onSingleTapConfirmed");
+        return true;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.gDetector.onTouchEvent(event);
+        // Be sure to call the superclass implementation
+        return super.onTouchEvent(event);
+    }
+
 }
